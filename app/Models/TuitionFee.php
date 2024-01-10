ad17571746ce7acd2c6235e3b33aac200070debc
anjumohanan1995 <anjumohanan@kawikatechnologies.com>
2024-01-10 17:29:42 +0530

    changes

--- app/Models/TuitionFee.php
+++ app/Models/TuitionFee.php
@@ -2,10 +2,45 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
 
-class TuitionFee extends Model
+class TuitionFee extends Eloquent
 {
     use HasFactory;
+
+    use SoftDeletes;
+
+    protected $connection = 'mongodb';
+    protected $collection = 'tuition_fee';
+
+    protected $fillable = [
+        'name',
+        'address',
+        'current_district',
+        'current_district_name',
+        'current_taluk',
+        'current_taluk_name',
+        'current_pincode',
+        'mobile',
+        'caste',
+        'relegion',
+        'annual_income',
+        'student_name','relation',
+        'school_name','class_number',
+        'tuition_center',
+        'place','signature',
+        'submitted_district',
+        'dist_name',
+        'submitted_teo',
+        'teo_name',
+        'date',
+        'user_id',
+        'status'
+    ];
+    public function districts() {
+        return $this->belongsTo('App\Models\District','district_id');
+    }
+
+
 }
