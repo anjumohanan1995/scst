35a97e560bd8eb0286536155580fd5b4bcec339e
anjumohanan1995 <anjumohanan@kawikatechnologies.com>
2024-02-12 16:12:16 +0530

    changes

---
+++ app/Http/Controllers/TDOMasterController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TDOMaster;
+use Illuminate\Http\Request;
+
+class TDOMasterController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\TDOMaster  $tDOMaster
+     * @return \Illuminate\Http\Response
+     */
+    public function show(TDOMaster $tDOMaster)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\TDOMaster  $tDOMaster
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(TDOMaster $tDOMaster)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\TDOMaster  $tDOMaster
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, TDOMaster $tDOMaster)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\TDOMaster  $tDOMaster
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(TDOMaster $tDOMaster)
+    {
+        //
+    }
+}
