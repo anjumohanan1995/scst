13d6924d1d8a49d125bd867e05ff7dd76fe80ae4
annieraj <annie@kawikatechnologies.com>
2024-02-16 15:51:59 +0530

    changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -584,12 +584,6 @@
                               
                             @endif
 
-                            @if (auth::user()->role === 'APO' || auth::user()->role === 'ATDO')
-                            
-                            @include('layouts.apo_atdo_app')
-                          
-                        @endif
-
 
                         </ul>
                         <div class="slide-right" id="slide-right">
